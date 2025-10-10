369739c37edfc9a4cebff03d0a6a8fa7423e08ce
asmi-g <asmi.gujral@gmail.com>
2025-10-10 14:34:40 -0400

    Added tilt option to elliptical orbit

--- poliastro_simulations/elliptical_orbit.py
+++ poliastro_simulations/elliptical_orbit.py
@@ -1,4 +1,6 @@
 import math
+import json
+from datetime import datetime, timedelta
 
 # Define constants as parameters
 mass_1 = 1
@@ -23,9 +25,10 @@ force_earth_g = universal_constant*mass_earth / (r_distance*r_distance)
 velocity_body = math.sqrt(universal_constant*mass_earth/r_distance)
 
 # Equation of Elliptical Orbit
-
 a = 7000000  # semi-major axis (X)
 b = 7000000  # semi-minor axis (Y)
+tilt_angle_deg = 5
+tilt_angle_rad = math.radians(tilt_angle_deg)
 period_seconds = 4800  # 90 minutes
 step = 600  # one position every 10 minutes
 positions = []
@@ -33,10 +36,17 @@ positions = []
 for t in range(0, period_seconds + 1, step):
     # θ from 0 to 2π
     theta = (2 * math.pi) * (t / period_seconds)
-    x = a * math.cos(theta)
-    y = b * math.sin(theta)
-    z = 0  # orbit lies in XY plane
-    positions += [t, x, y, z]
 
-# result: CZML list
+    # 2D Ellipse Parametric Equations
+    x = a * math.cos(theta) 
+    y = b * math.sin(theta) 
+    z = 0  # orbit lies in XY plane
+
+    # Rotate the ellipse around the X-axis (chosen arbitrarily) by the tilt angle
+    y_rot = y * math.cos(tilt_angle_rad) - z * math.sin(tilt_angle_rad)
+    z_rot = y * math.sin(tilt_angle_rad) + z * math.cos(tilt_angle_rad)
+
+    positions += [t, x, y_rot, z_rot]
+
+# result: CZML list of generated Cartesian Coordinates
 print(positions)
